d651324523cd48a36bb6f0fd7c3c3335d91a1f74
Minh Tran <minhtran.hust3801@gmail.com>
2021-11-06 01:01:09 +0700

    Add project files.

---
+++ ClothingShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClothingShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
