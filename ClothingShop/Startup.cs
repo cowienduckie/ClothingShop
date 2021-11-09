0dbbd448af5bd4824878d96310fb90a02253ba86
Minh Tran <minhtran.hust3801@gmail.com>
2021-11-10 00:26:53 +0700

    Setup Identity Framework for authentication - Add Identity package by Nuget - Modify DB Context - Register startup services - Clean up project

--- ClothingShop/Startup.cs
+++ ClothingShop/Startup.cs
@@ -1,15 +1,15 @@
+using ClothingShop.BusinessLogic.Repositories;
+using ClothingShop.BusinessLogic.Repositories.Interfaces;
+using ClothingShop.Entity.Data;
+using ClothingShop.Entity.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using ClothingShop.Entity.Data;
-using ClothingShop.BusinessLogic.Repositories;
-using ClothingShop.BusinessLogic.Repositories.Interfaces;
 using System;
-using System.IO;
 
 namespace ClothingShop
 {
@@ -33,6 +33,64 @@ namespace ClothingShop
             services.AddControllersWithViews();
 
             services.AddMvc();
+
+            //Identity Register
+            services.AddIdentity<Users, Roles>()
+                    .AddEntityFrameworkStores<ShopContext>()
+                    .AddDefaultTokenProviders();
+
+            //Identity Options
+            services.Configure<IdentityOptions>(options =>
+            {
+                //Config Password
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 1;
+
+                //Config Lockout
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                //Config User
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+
+                //Config Login
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+            //Config Cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                // options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.LoginPath = "/Account/Login/";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
+
+            services.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                options.ValidationInterval = TimeSpan.FromSeconds(5);
+            });
+
+            //Add authorization policy
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", policy =>
+                {
+                    policy.RequireRole("Admin");
+                });
+                options.AddPolicy("Customer", policy =>
+                {
+                    policy.RequireRole("Customer");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,11 +106,13 @@ namespace ClothingShop
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
