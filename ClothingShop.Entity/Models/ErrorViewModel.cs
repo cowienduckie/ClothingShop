ab2fbfeb8400ac1ce0e6070549a726c20c93c650
Minh Tran <minhtran.hust3801@gmail.com>
2021-11-21 19:57:25 +0700

    Revert "feature/Product"
    This reverts commit 361dcd5bc2026320cc39f725f292d9db48e57e45.

---
+++ ClothingShop.Entity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClothingShop.Entity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- ClothingShop/ClothingShop.Entity/Entities/Roles.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ClothingShop.Entity.Entities
-{
-    public class Roles : IdentityRole
-    {
-    }
-}
